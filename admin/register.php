5bd317b255f1f7f30f6dde4d874300e981e5af27
John Merigliano <john.merigliano@thefire.org>
2018-11-14 15:59:23 -0500

    working uploader

--- admin/register.php
+++ admin/register.php
@@ -5,5 +5,28 @@ $page = 'register';
 require_once('../inc/loader.php');
 require_once('../inc/header.php');
 require_once('../inc/nav.php');
+
+# set the value of the maximum value of the upload in bytes
+$max = 5000000;
+
+# move the file out of its temp location to another location
+if (isset($_POST['upload'])) {
+    # define the path to the upload folder
+    $destination = '../uploads/';
+    # call the upload class
+    require_once('../inc/uploader.php');
+    try {
+        $upload = new IsUpload($destination);
+        $upload->setMaxSize($max);
+        // $upload->addPermittedTypes(array('application/pdf', 'text/plain'));
+        # you can also set a permitted type if you just want one file type
+        # $upload->setPermittedTypes('text/plain');
+        $upload->move(true);
+        $upload_result = $upload->getMessages();
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
 require('../views/view-register.php');
 require_once('../inc/footer.php');
--- views/view-register.php
+++ views/view-register.php
@@ -1,1 +1,27 @@
-<p>upload images here</p>
+<div class="container">
+    <p>Upload new images to the store.</p>
+
+    <div class="row">
+        <div class="col-md-6">
+            <form action="<?php echo $urls['register']; ?>" method="post" enctype="multipart/form-data" id="uploadImage">
+                <div class="form-group">
+                    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max; ?>">
+                    <input type="file" name="image[]" id="image" multiple required>
+                </div>
+                <div class="form-group">
+                    <input class="btn btn-success" type="submit" name="upload" id="upload" value="upload">
+                </div>
+            </form>
+        </div>
+
+        <div class="col-md-6">
+            <?php if (isset($upload_result)): ?>
+            <ul>
+                <?php foreach ($upload_result as $u_msg): ?>
+                    <li><span class="text-success ist-file-names"><?php echo $u_msg; ?></span></li>
+                <?php endforeach; ?>
+            </ul>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
