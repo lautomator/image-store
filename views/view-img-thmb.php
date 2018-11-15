8d75ed04c0a71283fea1cc80e2e473dfa4889e54
John Merigliano <jmerigliano@gmail.com>
2018-11-15 18:29:06 -0500

    image path needs to be precise

--- views/view-img-thmb.php
+++ views/view-img-thmb.php
@@ -1,2 +1,2 @@
-<a href="<?php echo $urls['properties'] . '?img_id=' . $row['file_id']; ?>" title="<?php echo $row['file_name']; ?>"><img class="ist-thumb-img" src="<?php echo $uploads_dir . $row['file_name'] . '.' . $row['file_ext']; ?>"></a>
+<a href="<?php echo $urls['properties'] . '?img_id=' . $row['file_id']; ?>" title="<?php echo $row['file_name']; ?>"><img class="ist-thumb-img" src="<?php echo $row['file_path'] . $row['file_name'] . '.' . $row['file_ext']; ?>"></a>
 
