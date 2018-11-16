6ba3b1def59d66ac0e08d680543032bde3395ce0
John Merigliano <john.merigliano@thefire.org>
2018-11-16 11:49:36 -0500

    added condition for path url for all other pages

--- views/view-img-thmb.php
+++ views/view-img-thmb.php
@@ -1,2 +1,5 @@
-<a href="<?php echo $urls['properties'] . '?img_id=' . $row['file_id']; ?>" title="<?php echo $row['file_name']; ?>"><img class="ist-thumb-img" src="<?php echo $row['file_path'] . $row['file_name'] . '.' . $row['file_ext']; ?>"></a>
-
+<?php if ($page == 'home'): ?>
+    <a href="<?php echo $urls['properties'] . '?img_id=' . $row['file_id']; ?>" title="<?php echo $row['file_name']; ?>"><img class="ist-thumb-img" src="<?php echo $row['file_path'] . $row['file_name'] . '.' . $row['file_ext']; ?>"></a>
+<?php else: ?>
+    <a href="<?php echo $urls['properties'] . '?img_id=' . $row['file_id']; ?>" title="<?php echo $row['file_name']; ?>"><img class="ist-thumb-img" src="<?php echo '../' . $row['file_path'] . $row['file_name'] . '.' . $row['file_ext']; ?>"></a>
+<?php endif; ?>
