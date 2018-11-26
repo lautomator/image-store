4f636cb13e7e4bff7095fb237266edcdbb49321a
John Merigliano <john.merigliano@thefire.org>
2018-11-26 12:41:54 -0500

    all terms are now sorted on the search page

--- admin/search.php
+++ admin/search.php
@@ -11,5 +11,26 @@ if (isset($_GET['e'])) {
     $err_msg = 'Please select at least one tag.';
 }
 
+if (count($result['terms']) > 0) {
+    $all_terms = array();
+    $slugs = array();
+
+    // sort the slugs
+    foreach ($result['terms'] as $term) {
+        array_push($slugs, $term['term_slug']);
+    }
+    asort($slugs);
+
+    // sort the terms by the slugs
+    foreach ($slugs as $s) {
+        foreach ($result['terms'] as $t) {
+            if ($t['term_slug'] == $s) {
+                array_push($all_terms, $t);
+                break;
+            }
+        }
+    }
+}
+
 require('../views/view-search.php');
 require_once('../inc/footer.php');
--- views/view-tags-select-form.php
+++ views/view-tags-select-form.php
@@ -1,7 +1,7 @@
-<?php if (! empty($result['terms'])): ?>
+<?php if (count($all_terms) > 0): ?>
     <form method="post" action="<?php echo $home; ?>" class="form-inline">
         <div class="row">
-            <?php foreach ($result['terms'] as $terms): ?>
+            <?php foreach ($all_terms as $terms): ?>
                 <div class="col-md-3">
                     <div class="form-group">
                         <input type="checkbox" name="<?php echo $terms['term_slug'];?>" value="<?php echo $terms['term_id']; ?>">&nbsp;<?php echo $terms['term_name'];?>
